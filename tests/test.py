e655b0fcec1552ec23d31f4252bb82eac45cdfb8
LauraneC <laurane.charrier@outlook.com>
2024-04-25 17:34:38 +0200

    update test for inversion

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 import os
 import xarray as xr
 from ticoi.cube_data_classxr import cube_data_class
+from ticoi.inversion_functions import Construction_A_LP
 import numpy as np
 # name= f'{os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "test_data"))}/c_x18620_y08085_2016-2022_crop_GPS_Lower.nc'
 name = '/home/charriel/Documents/Bettik/Yukon/STACK/Data_2021/c_x18620_y08085_2016-2022_crop_GPS_Lower.nc'
@@ -11,4 +12,13 @@ cube.load(name)
 data, mean, dates_range = cube.load_pixel(1,2)
 data, mean, dates_range = cube.load_pixel(-138.18069, 60.29076)
 
+dates = np.array([['2013-03-14', '2013-03-30'], ['2013-03-14', '2013-03-30'], ['2013-03-14', '2013-04-15'],
+                  ['2013-03-30', '2013-04-15'],
+                  ['2013-03-30', '2013-04-15'], ['2013-03-14', '2013-08-13'], ['2013-03-14', '2013-10-16'],
+                  ['2013-06-19', '2013-07-13'],
+                  ['2013-03-14', '2013-10-24'], ['2013-03-14', '2013-11-01']]).astype('datetime64[D]')
+dates_range = np.array(
+    ['2013-03-14', '2013-03-30', '2013-04-15', '2013-06-19', '2013-07-13', '2013-08-13', '2013-10-16', '2013-10-24',
+     '2013-11-01']).astype('datetime64[D]')
+t=Construction_A_LP(dates,dates_range)
 # print(cube)
--- tests/test_inversion_functions.py
+++ tests/test_inversion_functions.py
@@ -1,20 +1,42 @@
-from ticoi.inversion_functions import Construction_dates_range_np
+from ticoi.inversion_functions import Construction_dates_range_np, Construction_A_LP
 import numpy as np
 
+class Test_inversion:
+    def setup_method(self):
+        # This method will run before each test
+        self.dates = np.array([
+            ['2013-03-14', '2013-03-30'], ['2013-03-14', '2013-03-30'], ['2013-03-14', '2013-04-15'],
+            ['2013-03-30', '2013-04-15'], ['2013-03-30', '2013-04-15'], ['2013-03-14', '2013-08-13'],
+            ['2013-03-14', '2013-10-16'], ['2013-06-19', '2013-07' '-13'], ['2013-03-14', '2013-10-24'],
+            ['2013-03-14', '2013-11-01']
+        ]).astype('datetime64[D]')
 
+        self.dates_range = np.array([
+            '2013-03-14', '2013-03-30', '2013-04-15', '2013-06-19', '2013-07-13', '2013-08-13',
+            '2013-10-16', '2013-10-24', '2013-11-01'
+        ]).astype('datetime64[D]')
 
-class test_inversion:
     def test_Construct_Dates_range(self):
-        dates = np.array([['2013-03-14', '2013-03-30'], ['2013-03-14', '2013-03-30'], ['2013-03-14', '2013-04-15'], ['2013-03-30', '2013-04-15'],
-         ['2013-03-30', '2013-04-15'], ['2013-03-14', '2013-08-13'], ['2013-03-14', '2013-10-16'], ['2013-06-19', '2013-07-13'],
-         ['2013-03-14', '2013-10-24'], ['2013-03-14', '2013-11-01']]).astype('datetime64[D]')
-        assert (Construction_dates_range_np(dates) == np.array(['2013-03-14', '2013-03-30', '2013-04-15', '2013-06-19', '2013-07-13', '2013-08-13', '2013-10-16', '2013-10-24', '2013-11-01']).astype('datetime64[D]')).all()
+        # Expected result
+        expected_dates_range = self.dates_range
 
-    def test_Construction_A_LP(dates, dates_range):
-        dates = np.array([['2013-03-14', '2013-03-30'], ['2013-03-14', '2013-03-30'], ['2013-03-14', '2013-04-15'],
-                          ['2013-03-30', '2013-04-15'],
-                          ['2013-03-30', '2013-04-15'], ['2013-03-14', '2013-08-13'], ['2013-03-14', '2013-10-16'],
-                          ['2013-06-19', '2013-07-13'],
-                          ['2013-03-14', '2013-10-24'], ['2013-03-14', '2013-11-01']]).astype('datetime64[D]')
-        dates_range = np.array(['2013-03-14', '2013-03-30', '2013-04-15', '2013-06-19', '2013-07-13', '2013-08-13', '2013-10-16', '2013-10-24', '2013-11-01']).astype('datetime64[D]')
+        # Run function under test
+        result = Construction_dates_range_np(self.dates)
 
+        # Assert the results are as expected
+        np.testing.assert_array_equal(result, expected_dates_range)
+
+    def test_Construction_A_LP(self):
+        # Expected result
+        expected = np.array([
+            [1, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1, 1, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1]
+        ])
+
+        # Run function under test
+        actual = Construction_A_LP(self.dates, self.dates_range)
+
+        # Assert the results are as expected
+        np.testing.assert_array_equal(actual, expected, err_msg="Construct A LP does not give the correct result")
