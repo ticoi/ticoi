aad642bba93a33c6611d7c13a9aafe01ab7a8b7f
Nathan Lioret <lioretn@ige-glacio-s192.u-ga.fr>
2024-05-06 12:07:09 +0200

    ticoi_cube_demo update

--- examples/ticoi_cube_demo.py
+++ examples/ticoi_cube_demo.py
@@ -12,57 +12,70 @@ Reference:
     Charrier, L., Yan, Y., Colin Koeniguer, E., Mouginot, J., Millan, R., & Trouvé, E. (2022). Fusion of multi-temporal and multi-sensor ice velocity observations.
     ISPRS annals of the photogrammetry, remote sensing and spatial information sciences, 3, 311-318.
 '''
-from ticoi.core import *
-from joblib import Parallel, delayed
+
 import time
-from ticoi.cube_data_classxr import cube_data_class
-import os
-from pyproj import CRS
-from tqdm import tqdm
 import itertools
 import warnings
+import os
+import numpy as np
+
+from joblib import Parallel, delayed
+from pyproj import CRS
+from tqdm import tqdm
+
+from ticoi.cube_data_classxr import cube_data_class
+from ticoi.core import process
+
+# %%========================================================================= #
+#                                    PARAMETERS                               #
+# =========================================================================%% #
 
 warnings.filterwarnings("ignore")
 
-# %%
-# Selection of data
-cube_name = '/media/tristan/Data3/Hala_lake/Landsat8/Hala_lake_diaplacement_LS7_subset.nc'  # Path where the Sentinel-2 IGE cubes are stored
-path_save = '/media/tristan/Data3/Hala_lake/Landsat8/ticoi_test/cube-demo/'  # Path where to stored the results
+####  Selection of data
+# cube_name = '/media/tristan/Data3/Hala_lake/Landsat8/Hala_lake_diaplacement_LS7_subset.nc'  # Path where the Sentinel-2 IGE cubes are stored
+# path_save = '/media/tristan/Data3/Hala_lake/Landsat8/ticoi_test/cube-demo/'  # Path where to stored the results
+cube_name = 'nathan/Donnees/Cubes_de_donnees/cubes_Sentinel_2/c_x01225_y03675_all_filt-multi.nc'
+path_save = 'nathan/Tests_MB/'
+name_save = 'test'
+proj = 'EPSG:32632'  # EPSG system of the coordinates given
+# To select a specific period for the measurements, if you want to select all the dates put None, 
+# else give an inteval of dates ['aaaa-mm-dd', 'aaaa-mm-dd'] ([min, max])
+dates_input = ['2015-01-01', '2023-01-01']
+# To select certain temporal baselines in the dataset, if you want to select all the temporal baselines put None, 
+# else give two integers [min, max] to form an interval in days
+temp_baseline = None
+sensor = None # Select a specific sensor 
+conf = False # If you want confidence indicators ranging between 0 and 1, with 1 the lowest errors
+unit = 'm/y' # Unit in m/y or m/d
+# If None, all the data are included ; if an integer, the data with an error higher than this integer are removed ;
+# if 'median_average', the data with a direction 45° away compared to the averaged direction are removed
+delete_outliers = None
+load_interp = 'nearest' # Interpolation used to select which data to use when the pixel is not exactly a pixel of the dataset
 
-
-proj = 'EPSG:32647'  # EPSG system of the coordinates given
-# To select a specific period for the measurements, if you want to select all the dates put None, else give an inteval of dates ['aaaa-mm-dd', 'aaaa-mm-dd'] ([min, max])
-dates_input = ['2000-01-01', '2014-12-31']
-
-temp_baseline = None  # to select certain temporal baselines in the dataset
-sensor = None
-conf = False  # if you want confidence indicators ranging between 0 and 1, with 1 the lowest errors
-unit = 'm/y'
-delete_outliers = None  # if None, all the data are included; if an integer, the data with a error higher than this interger are removed; if median_average, the data with a direction 45° away compared to the averaged direction are removed
-
-# Where to save the results
-name_result = 'cube_subset_test'  # name of the cube where to save the results
-path_save = f'/media/tristan/Data3/Hala_lake/Landsat8/ticoi_test/cube-demo/'  # folder where to save the results
+subset = [333615, 334529, 5079318, 5081236] # Bossons upper central part (741 points)
 
 ####  Inversion
-# Variables to play with
-smooth_method = 'gaussian' # Type of smoothing : 'gaussian', 'savgol', 'median', 'ewma' 
-coef = 200  # lambda : coef of the regularisation
-regu = '1accelnotnull'  # Type of regularisation : 1, 2,'1accelnotnull' : 1 is Tikhonov first order, 2 is Tikhonov second order and '1accelnotnull is Tikhonov first order with an apriori on the acceleration
+# Type of regularisation : 1, 2,'1accelnotnull','regu01' (1: Tikhonov first order, 2: Tikhonov second order,
+# '1accelnotnull': minimisation of the difference between the acceleration of the time series and acceleration computed on a moving average
+smooth_method = 'gaussian'
+regu = '1accelnotnull'
+coef = 100  # lambda : coef of the regularisation
 apriori_weight = True  # Add a weight in the first step of the inversion, True ou False
-# Varibales which can stay stable
 solver = 'LSMR_ini'  # Solver for the inversion : 'LSMR', 'LSMR_ini', 'LS', 'LS_bounded', 'LSQR'
 detect_temporal_decorrelation = True  # Detect temporal decorrelation by setting a weight of 0 at the beginning at the first inversion to all observation with a temporal baseline larger than 200
-# result_quality = None  # None or list of str, which can contain 'Norm_residual' to determine the L2 norm of the residuals from the last inversion, 'X_contribution' to determine the number of Y observations which have contributed to estimate each value in X (it corresponds to A.dot(weight))
-result_quality = ['X_contribution']
 
 ####  Interpolation
 option_interpol = 'spline'  # Type of interpolation : 'spline', 'nearest' or 'spline_smooth' for smoothing spline
-interpolation_bas = 90  # Temporal sampling of the velocity time series
-redundancy = 30
+interpolation_bas = 30  # Temporal sampling of the velocity time series
+redundancy = 5
+# result_quality = None  # None or list of str, which can contain 'Norm_residual' to determine the L2 norm of the residuals 
+# from the last inversion, 'X_contribution' to determine the number of Y observations which have contributed to estimate 
+# each value in X (it corresponds to A.dot(weight))
+result_quality = ['X_contribution']
 
-# Process
-nb_cpu = 40
+####  Process
+nb_cpu = 12
 verbose = False
 save = True
 interpolation = True
@@ -78,18 +91,27 @@ if not os.path.exists(path_save):
     os.mkdir(path_save)
 
 
-# %% Data download
-start_process = time.time()
+# %%========================================================================= #
+#                                 DATA DOWNLOAD                               #
+# =========================================================================%% #
+
+start = [time.time()]
+
 cube = cube_data_class()
 cube.load(cube_name, pick_date=dates_input, proj=proj, pick_temp_bas=temp_baseline,
-          pick_sensor=sensor, chunks={})
-print(f'Time download cube {round((time.time() - start_process), 4)} sec')
+          pick_sensor=sensor, subset=subset, chunks={})
 
-start = time.time()
-obs_filt = cube.preData_np(smooth_method=smooth_method, s_win=3, t_win=90, sigma=3, order=3, proj=proj, regu=regu,
-                           delete_outliers=None, verbose=False,
-                           velo_or_disp='velo')
-print(f'Time rolling_mean {round((time.time() - start), 4)} sec')
+stop = [time.time()]
+print(f'[ticoi_cube_demo] Cube of dimension (nz, nx, ny) Sentinel-2 : ({cube.nz}, {cube.nx}, {cube.ny}) ')
+print(f'[ticoi_cube_demo] Data loading took {round(stop[0] - start[0], 3)} s')
+
+
+start.append(time.time())
+obs_filt = cube.filter_cube(smooth_method=smooth_method, s_win=3, t_win=90, sigma=3, order=3, proj=proj, regu=regu,
+                            delete_outliers=None, verbose=False, velo_or_disp='velo')
+
+stop.append(time.time())    
+print(f'[ticoi_cube_demo] Filtering the cube took {round(stop[1] - start[1], 3)} s')
 
 cube_date1 = cube.date1_().tolist()
 cube_date1.remove(np.min(cube_date1))
@@ -97,12 +119,13 @@ merged = None
 start_date_interpol = np.min(cube_date1)
 last_date_interpol = np.max(cube.date2_())
 
-print(f'Cube of dimesion (nz,nx,ny) ITS_LIVE: ({cube.nz},{cube.nx},{cube.ny}) ')
 
-start = time.time()
+# %%========================================================================= #
+#                                INITIALISATION                               #
+# =========================================================================%% #
 
-# %% Initialisation of the cube to store the data
-start = time.time()
+start.append(time.time())
+
 if save:
     sensor_array = np.unique(cube.ds['sensor'])
 
@@ -111,7 +134,7 @@ if save:
     if merged is None:
         source = f'Temporal inversion on cube {cube.filename} using TICOI with a selection of the dates between: {dates_input}, with a selection of the temporal baselines {temp_baseline}'
     else:
-        source = f'Temporal inversion on cube {cube.filename} & {cube2.filename} using TICOI with a selection of the dates: {dates_input}, with a selection of the baseline {temp_baseline}, and a temporal spacing every {redundancy} days '
+        source = f'Temporal inversion on cube {cube.filename} using TICOI with a selection of the dates: {dates_input}, with a selection of the baseline {temp_baseline}, and a temporal spacing every {redundancy} days '
     if apriori_weight:
         source += ' and apriori weight'
     source += f'. The Tikhonov coef is: {coef}.'
@@ -123,49 +146,56 @@ if save:
 
 EPSG = f'EPSG:{CRS(cube.ds.proj4).to_epsg()}'
 
+stop.append(time.time())    
+print(f'[ticoi_cube_demo] Initialisation took {round(stop[2] - start[2], 3)} s')
 
+
+# %%========================================================================= #
+#                                      TICOI                                  #
+# =========================================================================%% #
+
+nb_points = len(cube.ds['x'].values) * len(cube.ds['y'].values)
+print(f'[ticoi_cube_demo] Number of CPU : {nb_cpu}')
+print(f'[ticoi_cube_demo] {nb_points} points to be computed within the given subset')
+
+start.append(time.time())
 
 xy_values = itertools.product(cube.ds['x'].values, cube.ds['y'].values)
-xy_values_tqdm = tqdm(xy_values, total=len(cube.ds['x'].values)*len(cube.ds['y'].values))
+xy_values_tqdm = tqdm(xy_values, total=len(cube.ds['x'].values)*len(cube.ds['y'].values), mininterval=0.1)
 
+# With parallelization
 result = Parallel(n_jobs=nb_cpu, verbose=0)(
-    delayed(process)(cube,
-        i, j, solver, coef, apriori_weight, path_save,obs_filt=obs_filt,interpolation_load_pixel='nearest', first_date_interpol=start_date_interpol,
-        last_date_interpol=last_date_interpol, conf=conf, regu=regu, interpolation_bas=interpolation_bas,
-        option_interpol=option_interpol, redundancy=redundancy,
-        detect_temporal_decorrelation=detect_temporal_decorrelation, unit=unit, result_quality=result_quality,
-        delete_outliers=delete_outliers, interpolation=interpolation,linear_operator=linear_operator,
-        verbose=verbose)
+    delayed(process)(cube, i, j, solver, coef, apriori_weight, path_save, 
+                      obs_filt=obs_filt, interpolation_load_pixel=load_interp,
+                      first_date_interpol=start_date_interpol, last_date_interpol=last_date_interpol, proj=EPSG,
+                      conf=conf, regu=regu, interpolation_bas=interpolation_bas, option_interpol=option_interpol, 
+                      redundancy=redundancy, detect_temporal_decorrelation=detect_temporal_decorrelation, 
+                      unit=unit, result_quality=result_quality, delete_outliers=delete_outliers, verbose=verbose)
     for i, j in xy_values_tqdm)
 
-
-# # %% Inversion
-# result = Parallel(n_jobs=nb_cpu, verbose=0)(
-#     delayed(process)(cube,
-#         i, j, solver, coef, apriori_weight, path_save,obs_filt=obs_filt,interpolation_load_pixel='nearest', first_date_interpol=start_date_interpol,
-#         last_date_interpol=last_date_interpol, conf=conf, regu=regu, interpolation_bas=interpolation_bas,
-#         option_interpol=option_interpol, redundancy=redundancy,
-#         detect_temporal_decorrelation=detect_temporal_decorrelation, unit=unit, result_quality=result_quality,
-#         delete_outliers=delete_outliers, interpolation=interpolation,linear_operator=linear_operator,
-#         verbose=verbose)
-#     for i in cube.ds['x'].values for j in cube.ds['y'].values)
-
-
-# For debuging, the version without parallelization
+# Without parallelization
 # result = []
-# for i in cube.ds['x'].values:
-#     for j in cube.ds['y'].values:
-#         result.append(process(
-#         cube.Load_Data(i, j, proj=EPSG, interp='linear', delete_outliers=delete_outliers, solver=solver,
-#                        regu=regu),
-#         i, j, solver, coef, apriori_weight, path_save, first_date_interpol=start_date_interpol,
-#         last_date_interpol=last_date_interpol, conf=conf, regu=regu, interpolation_bas=interpolation_bas,
-#         option_interpol=option_interpol, redundancy=redundancy,
-#             detect_temporal_decorrelation=detect_temporal_decorrelation, unit=unit,result_quality=result_quality, verbose=verbose))
-print(f'Time inversion {round((time.time() - start), 4)} sec')
+# for i, j in xy_values_tqdm:
+#     result.append(process(cube, i, j, solver, coef, apriori_weight, path_save, 
+#                           obs_filt=obs_filt, interpolation_load_pixel=load_interp,
+#                           first_date_interpol=start_date_interpol, last_date_interpol=last_date_interpol, proj=EPSG,
+#                           conf=conf, regu=regu, interpolation_bas=interpolation_bas, option_interpol=option_interpol, 
+#                           redundancy=redundancy, detect_temporal_decorrelation=detect_temporal_decorrelation, 
+#                           unit=unit, result_quality=result_quality, delete_outliers=delete_outliers, verbose=verbose))
 
-# %% save the res
-cube.write_result_TICOI(result, source, sensor, filename=name_result,
-                        savepath=path_save, result_quality=result_quality, verbose=verbose)
-print(f'Total process {(time.time() - start_process) / 60} min')
-print('stop')
+stop.append(time.time())
+print(f'[ticoi_cube_demo] TICOI processing took {round(stop[3] - start[3], 3)} s')
+
+
+# %%========================================================================= #
+#                                WRITING RESULTS                              #
+# =========================================================================%% #
+
+start.append(time.time())
+
+cube.write_result_ticoi(result, source, sensor, filename=name_save, savepath=path_save, 
+                        result_quality=result_quality, verbose=verbose)
+
+stop.append(time.time())
+print(f'[ticoi_cube_demo] Writing cube to netCDF file took {round(stop[4] - start[4], 3)} s')
+print(f'[ticoi_cube_demo] Overall processing took {round(stop[4] - start[0], 0)} s')
