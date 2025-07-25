89481739968aff0279c2dd217c5079807a2ffa9b
TristanBlus <tristanblus@outlook.com>
2025-07-25 17:17:10 +0800

    Revert "reconstruct the result writing function with numpy vectorization"
    This reverts commit a7c3f11179b24c9139ed51b366e36d3d7e489b7e.

--- examples/basic/python_script/cube_ticoi_demo.py
+++ examples/basic/python_script/cube_ticoi_demo.py
@@ -25,7 +25,7 @@ from joblib import Parallel, delayed
 from tqdm import tqdm
 
 from ticoi.core import process, process_blocks_refine, save_cube_parameters
-from ticoi.cube_data_classxr import CubeDataClass, CubeResultsWriter
+from ticoi.cube_data_classxr import CubeDataClass
 
 warnings.filterwarnings("ignore")
 
@@ -236,42 +236,6 @@ if save:  # Save TICOI results to a netCDF file, thus obtaining a new data cube
             filename=f"{result_fn}_interp" if several else result_fn,
             savepath=path_save if save else None,
             result_quality=inversion_kwargs["result_quality"],
-            verbose=inversion_kwargs["verbose"],
-        )
-
-# Plot the mean velocity as an example
-if save_mean_velocity and cube_interp is not None:
-    cube_interp.average_cube(return_format="geotiff", return_variable=["vv"], save=True, path_save=path_save)
-
-stop.append(time.time())
-if save or save_mean_velocity:
-    print(f"[cube_ticoi_demo] Results saved at {path_save}")
-    print(f"[cube_ticoi_demo] Writing cube to netCDF file took {round(stop[-1] - start[-1], 3)} s")
-
-start.append(time.time())
-
-if save:  # Save TICOI results to a netCDF file, thus obtaining a new data cube
-    print(f"[cube_ticoi_demo] Writing results to netCDF file with result_writer")
-    several = type(returned) == list and len(returned) >= 2
-    writer = CubeResultsWriter(cube)
-    if "invert" in returned:
-        cube_invert = writer.write_result_tico(
-            result["invert"] if several else result,
-            source,
-            sensor,
-            result_quality=inversion_kwargs["result_quality"],
-            filename=f"{result_fn}_invert" if several else result_fn,
-            savepath=path_save if save else None,
-            verbose=inversion_kwargs["verbose"],
-        )
-    if "interp" in returned:
-        cube_interp = writer.write_result_ticoi(
-            result["interp"] if several else result,
-            source_interp,
-            sensor,
-            result_quality=inversion_kwargs["result_quality"],
-            filename=f"{result_fn}_interp" if several else result_fn,
-            savepath=path_save if save else None,
             verbose=inversion_kwargs["verbose"],
         )
 
