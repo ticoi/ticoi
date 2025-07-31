9fc0a4b02c2baf27502fe2b2355d9fd5cf5de4b2
LauraneC <laurane.charrier@outlook.com>
2025-07-31 11:21:28 +0200

    git add Lowell

--- tests/test_example.py
+++ tests/test_example.py
@@ -7,10 +7,10 @@ def test_example_script_output():
     cube_demo_simple = f"{os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'examples', 'basic', 'python_script'))}/cube_ticoi_demo.py"
     cube_demo_its_live = f"{os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'examples', 'advanced'))}/cube_ticoi_demo_its_live.py"
 
-    result = subprocess.run(["python", cube_demo_simple], capture_output=True, text=True)
+    result = subprocess.run(["python3", cube_demo_simple], capture_output=True, text=True)
     # Check if the script ran successfully
     assert result.returncode == 0, "The example cube_demo_simple has crashed"
 
-    result = subprocess.run(["python", cube_demo_its_live], capture_output=True, text=True)
+    result = subprocess.run(["python3", cube_demo_its_live], capture_output=True, text=True)
     # Check if the script ran successfully
     assert result.returncode == 0, "The example cube_demo_its_live has crashed"
