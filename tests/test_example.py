2a7c2b4dfa7b1484a1d02dfe216516ca29715266
LauraneC <laurane.charrier@outlook.com>
2025-08-04 14:46:54 +0200

    add run of jupernotebook for second notebook

--- tests/test_example.py
+++ tests/test_example.py
@@ -6,6 +6,8 @@ def test_example_script_output():
     # Run the example.py script
     cube_demo_simple = f"{os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'examples', 'basic', 'python_script'))}/cube_ticoi_demo.py"
     pixel_demo_simple = f"{os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'examples', 'basic', 'notebook'))}/pixel_demo_local_ncdata.ipynb"
+    pixel_demo_simple_cloud = f"{os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'examples', 'basic', 'notebook'))}/pixel_demo_its_live_on_cloud.ipynb"
+
     # cube_demo_its_live = f"{os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'examples', 'advanced'))}/cube_ticoi_demo_its_live.py"
 
     result = subprocess.run(["python3", cube_demo_simple], capture_output=True, text=True)
@@ -17,4 +19,11 @@ def test_example_script_output():
         capture_output=True,
         text=True,
     )
-    assert result.returncode == 0, f"The notebook pixel_demo_simple crashed:\n{result.stderr}"
+    assert result.returncode == 0, f"The notebook /pixel_demo_local_ncdata crashed:\n{result.stderr}"
+
+    result = subprocess.run(
+        ["jupyter", "nbconvert", "--to", "notebook", "--execute", "--inplace", pixel_demo_simple_cloud],
+        capture_output=True,
+        text=True,
+    )
+    assert result.returncode == 0, f"The notebook pixel_demo_its_live_on_cloud crashed:\n{result.stderr}"
